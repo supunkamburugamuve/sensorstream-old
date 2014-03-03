e8ab1c34ac0005e01b2d08354beb35ebc523741e
Supun Kamburugamuva <supun@149-160-147-252.dhcp-bl.indiana.edu>
2014-03-03 15:47:09 -0500

    adding more functionality

--- sensorstream-core/src/main/java/cgl/sensorstream/updates/UpdateListener.java
+++ sensorstream-core/src/main/java/cgl/sensorstream/updates/UpdateListener.java
@@ -1,5 +1,57 @@
 package cgl.sensorstream.updates;
 
-public class UpdateListener {
+import org.apache.activemq.spring.ActiveMQConnectionFactory;
 
+import javax.jms.*;
+
+public class UpdateListener {
+    private String queueName;
+
+    private String connectionFactory;
+
+    public class ListenerWorker implements Runnable,  ExceptionListener{
+        public void run() {
+            try {
+                // Create a ConnectionFactory
+                ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+
+                // Create a Connection
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
+
+                connection.setExceptionListener(this);
+
+                // Create a Session
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                // Create the destination (Topic or Queue)
+                Destination destination = session.createQueue("TEST.FOO");
+
+                // Create a MessageConsumer from the Session to the Topic or Queue
+                MessageConsumer consumer = session.createConsumer(destination);
+
+                // Wait for a message
+                Message message = consumer.receive(1000);
+
+                if (message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    String text = textMessage.getText();
+                    System.out.println("Received: " + text);
+                } else {
+                    System.out.println("Received: " + message);
+                }
+
+                consumer.close();
+                session.close();
+                connection.close();
+            } catch (Exception e) {
+                System.out.println("Caught: " + e);
+                e.printStackTrace();
+            }
+        }
+
+        public synchronized void onException(JMSException ex) {
+            System.out.println("JMS Exception occured.  Shutting down client.");
+        }
+    }
 }
