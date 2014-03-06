373feb2eca8d4725ed7f8a63d90e9c35f6186907
Supun <supun@supun-All-Series.(none)>
2014-03-06 13:54:04 -0500

    adding more

--- sensorstream-core/src/main/java/cgl/sensorstream/core/SensorStreamMain.java
+++ sensorstream-core/src/main/java/cgl/sensorstream/core/SensorStreamMain.java
@@ -10,15 +10,26 @@ import java.util.concurrent.BlockingQueue;
 public class SensorStreamMain {
     public static void main(String[] args) {
         // read the configuration
+
         Map conf = Utils.readDefaultConfig();
 
         BlockingQueue<Update> updates = new ArrayBlockingQueue<Update>((Integer) conf.get(Configuration.SS_SENSOR_UPDATES_SIZE));
+        ZooKeeperUpdater zkUpdater = null;
+        UpdateListener listener = null;
+        try {
+            // now try to connect to zookeeper
+            zkUpdater = new ZooKeeperUpdater();
+            zkUpdater.start(conf, updates);
 
-        // now try to connect to zookeeper
-        ZooKeeperUpdater zkUpdater = new ZooKeeperUpdater();
-        zkUpdater.start(conf, updates);
-
-        // now create the listener for updates
-        UpdateListener listener = new UpdateListener(conf, new Updater(updates));
+            // now create the listener for updates
+            listener = new UpdateListener(conf, new Updater(updates));
+        } finally {
+            if (zkUpdater != null) {
+                zkUpdater.stop();
+            }
+            if (listener != null) {
+                listener.destroy();
+            }
+        }
     }
 }
