62748486a2289356f235e761f8c984a1944634ce
Supun <supun@supun-All-Series.(none)>
2014-03-06 08:52:03 -0500

    adding more

--- sensorstream-core/src/main/java/cgl/sensorstream/core/ZooKeeperUpdater.java
+++ sensorstream-core/src/main/java/cgl/sensorstream/core/ZooKeeperUpdater.java
@@ -1,17 +1,26 @@
 package cgl.sensorstream.core;
 
 import cgl.sensorstream.core.config.Configuration;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.utils.CloseableUtils;
 
+import javax.management.remote.rmi.RMIServer;
 import java.util.List;
 import java.util.Map;
 
 public class ZooKeeperUpdater {
+    private CuratorFramework client = null;
+    private PathChildrenCache cache = null;
+
     public void start(Map conf) {
         int port = (int) conf.get(Configuration.SS_ZOOKEEPER_PORT);
         List servers = (List) conf.get(Configuration.SS_ZOOKEEPER_SERVERS);
 
-        CuratorFramework    client = null;
-        PathChildrenCache   cache = null;
+        CuratorFramework client = null;
+        PathChildrenCache cache = null;
         try
         {
             client = CuratorFrameworkFactory.newClient(server.getConnectString(), new ExponentialBackoffRetry(1000, 3));
@@ -22,12 +31,14 @@ public class ZooKeeperUpdater {
             cache.start();
 
             processCommands(client, cache);
-        }
-        finally
-        {
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
             CloseableUtils.closeQuietly(cache);
             CloseableUtils.closeQuietly(client);
             CloseableUtils.closeQuietly(server);
         }
     }
+
+    
 }
