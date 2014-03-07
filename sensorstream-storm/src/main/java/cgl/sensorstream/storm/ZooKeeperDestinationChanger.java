6aa8e6dd08d1ff5e967ed6e09dd698784e3bad6c
Supun <supun@supun-All-Series.(none)>
2014-03-06 23:35:32 -0500

    adding code for zookeeper path cache

--- sensorstream-storm/src/main/java/cgl/sensorstream/storm/ZooKeeperDestinationChanger.java
+++ sensorstream-storm/src/main/java/cgl/sensorstream/storm/ZooKeeperDestinationChanger.java
@@ -4,6 +4,7 @@ import backtype.storm.contrib.jms.DestinationChanger;
 import backtype.storm.contrib.jms.Notification;
 import cgl.sensorstream.core.Utils;
 import cgl.sensorstream.core.config.Configuration;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.cache.PathChildrenCache;
@@ -11,10 +12,14 @@ import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.utils.CloseableUtils;
-import org.apache.curator.utils.ZKPaths;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -22,12 +27,29 @@ import java.util.concurrent.BlockingQueue;
 public class ZooKeeperDestinationChanger implements DestinationChanger {
     private static Logger LOG = LoggerFactory.getLogger(ZooKeeperDestinationChanger.class);
 
+    private ActiveMQConnectionFactory connectionFactory;
+    private Connection connection = null;
+    private Session session;
+
     private BlockingQueue<Notification> notifications;
 
     private CuratorFramework client = null;
     private PathChildrenCache cache = null;
 
     public void start(Map conf) {
+        // Create a ConnectionFactory
+        try {
+            connectionFactory = new ActiveMQConnectionFactory((String) conf.get(Configuration.SS_BROKER_URL));
+            connection = connectionFactory.createConnection();
+            connection.start();
+            // Create a Session
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        } catch (JMSException e) {
+            String msg = "Error connecting to the broker";
+            LOG.error(msg);
+            throw new RuntimeException(msg, e);
+        }
+
         notifications = new ArrayBlockingQueue<Notification>((Integer)
                 conf.get(Configuration.SS_SENSOR_UPDATES_SIZE));
 
@@ -48,28 +70,55 @@ public class ZooKeeperDestinationChanger implements DestinationChanger {
         addListener(cache);
     }
 
-    private static void addListener(PathChildrenCache cache) {
+    private void addListener(PathChildrenCache cache) {
         // a PathChildrenCacheListener is optional. Here, it's used just to log changes
         PathChildrenCacheListener listener = new PathChildrenCacheListener() {
             @Override
             public void childEvent(CuratorFramework client, PathChildrenCacheEvent event) throws Exception {
                 switch (event.getType()) {
                     case CHILD_ADDED: {
-                        System.out.println("Node added: " + ZKPaths.getNodeFromPath(event.getData().getPath()));
+                        Notification notification = new Notification(
+                                event.getData().getPath(),
+                                createDestination(getDestinationPath(event.getData().getData())),
+                                Notification.Type.ADD);
+                        notifications.put(notification);
                         break;
                     }
                     case CHILD_UPDATED: {
-                        System.out.println("Node changed: " + ZKPaths.getNodeFromPath(event.getData().getPath()));
+                        LOG.debug("Received update notification, ignoring..: ", event.getData().getPath());
                         break;
                     }
                     case CHILD_REMOVED: {
-                        System.out.println("Node removed: " + ZKPaths.getNodeFromPath(event.getData().getPath()));
+                        Notification notification = new Notification(
+                                event.getData().getPath(),
+                                createDestination(getDestinationPath(event.getData().getData())),
+                                Notification.Type.REMOVE);
+                        notifications.put(notification);
                         break;
                     }
                 }
             }
         };
         cache.getListenable().addListener(listener);
+    }
+
+    private String getDestinationPath(byte []data) {
+        throw new NotImplementedException();
+    }
+
+    private Destination createDestination(String path) {
+        // Create a Connection
+        try {
+
+            // Create the destination (Topic or Queue)
+            Destination destination = session.createQueue(path);
+
+            return destination;
+        } catch (JMSException e) {
+            String msg = "Error creating the JMS Destination: " + path;
+            LOG.error(msg, e);
+            throw new RuntimeException(msg, e);
+        }
     }
 
     @Override
