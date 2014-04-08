847253a43ea648330e12ffdaf8906f200df03d5c
chathuri <chathuri@apache.org>
2014-04-08 08:41:23 -0600

    adding more

--- sensorstream-storm/src/main/java/cgl/sensorstream/storm/perf/PerfTopology.java
+++ sensorstream-storm/src/main/java/cgl/sensorstream/storm/perf/PerfTopology.java
@@ -16,13 +16,25 @@ import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 public class PerfTopology {
-    public static class ExclamationBolt extends BaseRichBolt {
+    public static class PerfAggrBolt extends BaseRichBolt {
+        private static Logger LOG = LoggerFactory.getLogger(PerfAggrBolt.class);
         OutputCollector _collector;
+
+        long averageLatency = 0;
+
+        long count = 0;
 
         @Override
         public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
@@ -31,7 +43,13 @@ public class PerfTopology {
 
         @Override
         public void execute(Tuple tuple) {
-            tuple.getValue(10);
+            Long val = (Long) tuple.getValue(0);
+            count++;
+            long delta = val - averageLatency;
+            averageLatency = averageLatency + delta / count;
+            _collector.emit(new Values(averageLatency));
+
+            LOG.info("The latency: " + val + " count: " + count);
             _collector.ack(tuple);
         }
 
@@ -45,27 +63,13 @@ public class PerfTopology {
         TopologyBuilder builder = new TopologyBuilder();
 
         JmsSpout spout = new JmsSpout();
-        spout.setJmsProvider(new JmsProvider() {
-            @Override
-            public ConnectionFactory connectionFactory() throws Exception {
-                // Create a ConnectionFactory
-                ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
-
-                return connectionFactory;
-            }
-
-            @Override
-            public Destination destination() throws Exception {
-
-                return null;
-            }
-        });
+        spout.setJmsProvider(new LocalJMSProvider());
 
         spout.setJmsTupleProducer(new JmsTupleProducer() {
             @Override
             public Values toTuple(Message message) throws JMSException {
                 if (message instanceof TextMessage) {
-                    long time = ((TextMessage) message).getLongProperty("time");
+                    long time = message.getLongProperty("time");
                     return new Values(System.currentTimeMillis() - time);
                 } else {
                     return null;
@@ -78,21 +82,50 @@ public class PerfTopology {
             }
         });
 
-        builder.setSpout("word", spout, 3);
-        builder.setBolt("time", new ExclamationBolt(), 3).shuffleGrouping("time");
+        builder.setSpout("word", spout, 2);
+        builder.setBolt("time1", new PerfAggrBolt(), 2).shuffleGrouping("word");
 
         Config conf = new Config();
         conf.setDebug(true);
 
         if (args != null && args.length > 0) {
-            conf.setNumWorkers(4);
+            conf.setNumWorkers(6);
             StormSubmitter.submitTopology("perf", conf, builder.createTopology());
         } else {
             LocalCluster cluster = new LocalCluster();
             cluster.submitTopology("perf", conf, builder.createTopology());
-            Utils.sleep(10000);
+            Thread.sleep(60000);
             cluster.killTopology("perf");
             cluster.shutdown();
+        }
+    }
+
+    private static class LocalJMSProvider implements JmsProvider {
+        ConnectionFactory connectionFactory;
+        Destination destination;
+
+        private LocalJMSProvider() {
+            this.connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+            Connection connection;
+            try {
+                connection = connectionFactory.createConnection();
+                connection.start();
+
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                this.destination = session.createQueue("send");
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public ConnectionFactory connectionFactory() throws Exception {
+            return connectionFactory;
+        }
+
+        @Override
+        public Destination destination() throws Exception {
+            return destination;
         }
     }
 }
