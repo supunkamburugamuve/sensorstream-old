c7c349c679d93f248c72f240f9057d19fdeb56c4
chathuri <chathuri@apache.org>
2014-04-08 20:47:24 -0600

    adding more

--- sensorstream-storm/src/main/java/cgl/sensorstream/storm/perf/PerfTopology.java
+++ sensorstream-storm/src/main/java/cgl/sensorstream/storm/perf/PerfTopology.java
@@ -6,59 +6,15 @@ import backtype.storm.StormSubmitter;
 import backtype.storm.contrib.jms.JmsProvider;
 import backtype.storm.contrib.jms.JmsTupleProducer;
 import backtype.storm.contrib.jms.spout.JmsSpout;
-import backtype.storm.task.OutputCollector;
-import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
-import backtype.storm.utils.Utils;
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.jms.*;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
 
 public class PerfTopology {
-    public static class PerfAggrBolt extends BaseRichBolt {
-        private static Logger LOG = LoggerFactory.getLogger(PerfAggrBolt.class);
-        OutputCollector _collector;
-
-        double averageLatency = 0;
-
-        long count = 0;
-
-        @Override
-        public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
-            _collector = collector;
-        }
-
-        @Override
-        public void execute(Tuple tuple) {
-            Long val = (Long) tuple.getValue(0);
-            count++;
-            double delta = val - averageLatency;
-            averageLatency = averageLatency + delta / count;
-            _collector.emit(new Values(averageLatency));
-
-            LOG.info("The latency: " + averageLatency + " count: " + count);
-            _collector.ack(tuple);
-        }
-
-        @Override
-        public void declareOutputFields(OutputFieldsDeclarer declarer) {
-            declarer.declare(new Fields("time"));
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         TopologyBuilder builder = new TopologyBuilder();
 
